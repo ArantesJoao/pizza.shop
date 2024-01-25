19beb53f8855ec816df35bde7561140ab4e94a60
João <arantesjoaov@gmail.com>
2024-01-25 09:42:11 -0300

    adding layouts and react-helmet

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,3 +1,11 @@
+import { Helmet } from 'react-helmet-async'
+
 export function Dashboard() {
-  return <h1 className="">Dashboard</h1>
+  return (
+    <>
+      <Helmet title="Dashboard" />
+
+      <h1>Dashboard</h1>
+    </>
+  )
 }
