8922e0ae9940bf2cc3c91c92fcf976307635fdb5
João <arantesjoaov@gmail.com>
2024-02-15 13:26:03 -0300

    feat: creating mocks for profile getters and setters

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -4,28 +4,33 @@ import { env } from '@/env'
 
 import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
 import { getDayOrdersAmountMock } from './get-day-orders-amount-mock'
+import { getManagedRestaurantMock } from './get-managed-restaurant-mock'
 import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
 import { getMonthOrdersAmountMock } from './get-month-orders-amount-mock'
 import { getMonthRevenueMock } from './get-month-revenue-mock'
 import { getPopularProductsMock } from './get-popular-products-mock'
+import { getProfileMock } from './get-profile-mock'
 import { registerRestaurantMock } from './register-restaurant-mock'
 import { signInMock } from './sign-in-mock'
+import { updateProfileMock } from './update-profile-mock'
 
 export const worker = setupWorker(
-  getDayOrdersAmountMock,
-  getMonthCanceledOrdersAmountMock,
-  getMonthOrdersAmountMock,
-  getMonthRevenueMock,
-  registerRestaurantMock,
   signInMock,
+  registerRestaurantMock,
+  getDayOrdersAmountMock,
+  getMonthOrdersAmountMock,
+  getMonthCanceledOrdersAmountMock,
+  getMonthRevenueMock,
   getDailyRevenueInPeriodMock,
   getPopularProductsMock,
+  getProfileMock,
+  getManagedRestaurantMock,
+  updateProfileMock,
 )
 
 export async function enableMSW() {
   if (env.MODE !== 'test') {
     return
   }
-
   await worker.start()
 }
