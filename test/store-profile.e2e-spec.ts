2652f3f00fa69117acad0f028f18af3540fdb6a8
João <arantesjoaov@gmail.com>
2024-02-19 14:56:27 -0300

    feat: creating profile update e2e tests

---
+++ test/store-profile.e2e-spec.ts
@@ -0,0 +1,25 @@
+import { expect, test } from '@playwright/test'
+
+test('update profile successfully', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await page.getByRole('button', { name: 'Pizza Shop' }).click()
+  await page.getByRole('menuitem', { name: 'Store profile' }).click()
+
+  await page.getByLabel('Name').fill('Pizza Shop 2')
+  await page.getByLabel('Description').fill('New description')
+
+  await page.getByRole('button', { name: 'Save' }).click()
+
+  await page.waitForLoadState('networkidle')
+
+  const toast = page.getByText('Profile updated successfully!')
+
+  expect(toast).toBeVisible()
+
+  await page.getByRole('button', { name: 'Close' }).click()
+
+  await page.waitForTimeout(150)
+
+  expect(page.getByRole('button', { name: 'Pizza Shop 2' })).toBeVisible()
+})
