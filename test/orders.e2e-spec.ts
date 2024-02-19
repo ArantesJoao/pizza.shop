a9db50e2fad87ca9074365ca43d7f1ecb297b3c7
João <arantesjoaov@gmail.com>
2024-02-19 15:21:38 -0300

    fix: missing async on expect

--- test/orders.e2e-spec.ts
+++ test/orders.e2e-spec.ts
@@ -61,7 +61,7 @@ test('filter by status', async ({ page }) => {
   await page.getByLabel('Canceled').click()
   await page.getByRole('button', { name: 'Filter results' }).click()
 
-  const tableRows = await page.getByRole('cell', { name: 'Canceled' }).all()
+  const tableRows = await page.getByRole('cell', { name: 'Canceled' })
 
-  expect(tableRows).toHaveLength(10)
+  await expect(tableRows).toHaveCount(10)
 })
